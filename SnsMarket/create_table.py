488c441a986b2194ca32d02bb35460138fc55d9b
Aaron Roh <roharon98@gmail.com>
2023-06-26 07:56:21 +0900

    Modify: create_table.py market_id NULL Constraint

--- SnsMarket/create_table.py
+++ SnsMarket/create_table.py
@@ -12,13 +12,15 @@ Order.__table__.create(bind=engine, checkfirst=True)
 User.__table__.create(bind=engine, checkfirst=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+db = SessionLocal()
 
 market = Market(name="첫번째 가게", certificated=True)
+db.add(market)
+db.commit()
+
 item = Item(name="연필", price="100", quantity="5", market_id=market.id)
 user = User(name="노아론")
 
-db = SessionLocal()
-db.add(market)
 db.add(item)
 db.add(user)
 db.commit()
