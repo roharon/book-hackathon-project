3518179e0261af7b8b8e95b2e40c0c9d49b0eb48
roharon <roharon98@gmail.com>
2024-02-05 01:16:20 +0900

    Modify: pip build target to Python 3.12

--- GroceryStore/authorizer/build.sh
+++ GroceryStore/authorizer/build.sh
@@ -1,5 +1,5 @@
 mkdir ./.build
-pip install --platform manylinux2014_aarch64 --target=./.build --implementation cp --python-version 3.11 --only-binary=:all: --upgrade -r requirements.txt
+pip install --platform manylinux2014_aarch64 --target=./.build --implementation cp --python-version 3.12 --only-binary=:all: --upgrade -r requirements.txt
 
 cp ./lambda_function.py ./.build
 cd ./.build
